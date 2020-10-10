d566bb5a11bb464bf17c955c2d0d5410d213a918
breshwari1999 <72658533+breshwari1999@users.noreply.github.com>
2020-10-10 19:13:19 +0530

    Add files via upload

---
+++ dot.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i,j,n;
+	for(i=1;i<=9;i++){
+	
+		for(j=1;j<=5;j++){
+		printf("%d\t",j);
+		}
+		printf("\n");
+
+	}
+return(0);
+	getch();
+
+}
